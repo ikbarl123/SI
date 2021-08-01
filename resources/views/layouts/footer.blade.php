871770358f9a87179949711155d1e0f5c0c0025b
ikbarl123 <ikbarl123@gmail.com>
2021-08-01 13:55:23 +0700

    2

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="main-footer">
         <div class="footer-left">
-          Copyright &copy; 2021 <b>10119294-Lingga Juliansyah-IF7 </b> <div class="bullet"></div> Template <a href="https://getstisla.com/">Stisla</a>
+          Copyright &copy; 2021 <b> </b> <div class="bullet"></div> Template <a href="https://getstisla.com/">Stisla</a>
         </div>
         <div class="footer-right">
          0.0.1
